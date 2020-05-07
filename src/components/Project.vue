cbf3d40654db9470f1583ec666e7b196a2b46cb1
James Enteria <enteria.jeremiah@gmail.com>
2020-05-08 03:25:46 +0800

    needs modal

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,17 +1,47 @@
 
 
 <template>
-	<v-scale-transition mode="in-out" origin="center center">
-	<div class="col-md-4 col-12">
-			<img class="gallery_img img-responsive" :src="project.source">
-		</div>
-    </v-scale-transition>
+	<div>
+		<v-hover>
+			<template v-slot:default="{ hover }">
+				<div class="col-sm-4 col-12" >
+					<img class="gallery_img img-responsive"  :src="project.source">
+					<v-fade-transition>
+						<v-overlay v-if="hover" absolute :opacity="0.90" class="text-center">
+							<h1>{{project.name}}</h1>
+							<p style="cursor: pointer;">-click for more info-</p>
+						</v-overlay>
+					</v-fade-transition>
+					
+				</div>
+			</template>
+		</v-hover>
+
+		
+		
+	</div>
+	
+		
+	
 	
 </template>
 
 <script>	
 export default {
-	props: ['project']
+	props: ['project'],
+	data() {
+		return {
+			absolute: true,
+			overlay: false
+		}
+	},
+	methods: {
+		setToFalse() {
+			setTimeout(()=> {
+				return false;
+			}, 2000)
+		}
+	}
     
 }
 </script>
@@ -20,7 +50,10 @@ export default {
 	.gallery_img {
     width: 100%;
     height: 100%;
-    object-fit: cover;
+     object-fit: cover; 
+    
+
 }
 
+	
 </style>
