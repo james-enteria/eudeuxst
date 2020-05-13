a5853cf409609511dfe68b70e7bbe7e103cdec79
James Enteria <enteria.jeremiah@gmail.com>
2020-05-13 18:30:40 +0800

    needs progress on contact page

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,21 +1,101 @@
 <template>
-  
+  <div>
+    
 
-<v-btn @click="alertMe" light data-toggle="modal" data-target="'#modal'" >click me for info </v-btn>
+  <div class="row text-center justify-content-center">
+    
+    <v-form
+      ref="form"
+      @submit.prevent="sendEmail"
+      method="post"
+      
+    >
+      <v-text-field
+        v-model="name"
+        :counter="30"
+        :rules="nameRules"
+        label="Name"
+        required
+        outlined
+        name="user_name"
+      ></v-text-field>
 
-            
-          <div class="modal fade" id="'#modal'" tabindex="-1" role="dialog"  aria-hidden="true">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title">Area PE 1</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              
-            </div>
-          </div>
-        </div>  
-        </div>
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="E-mail"
+        required
+        outlined
+        name="user_email"
+      ></v-text-field>
+
+      <v-textarea
+      v-model="message"
+          outlined
+          label="Message"
+          name="message"
+          type="submit"
+          
+          
+        ></v-textarea>
+
+
+      <v-btn
+        :disabled="valid"
+        color="success"
+        class="mr-4"
+        type="submit"
+        :loading="valid"
+      >
+        send
+      </v-btn>
+
+
+
+    </v-form>
+  </div>
+
+  </div>
 </template>
+
+<script>
+import emailjs from 'emailjs-com';
+
+export default {
+  data: () => ({
+      valid: false,
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 30) || 'Name must be less than 30 characters',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      message: ''
+    }),
+  methods: {
+    validate () {
+        this.$refs.form.validate()
+    },
+    sendEmail: (e) => {
+      
+       //will initiate loading animation on SEND button
+
+      emailjs.sendForm('gmail', 'template_WZgAcJwW', e.target, 'user_mOT0OhVnn157XyTvKqUMY')
+        .then((result) => {
+            console.log('SUCCESS!', result.status, result.text);
+
+        }, (error) => {
+            console.log('FAILED...', error);
+
+        });
+      this.name = "";
+      this.email = "";
+      this.message = "";
+    }
+  }
+}
+</script>
--- src/components/Project.vue
+++ src/components/Project.vue
@@ -4,28 +4,47 @@
 	
 		<v-hover>
 			<template v-slot:default="{ hover }">
-				<div class="col-sm-4 col-12" >
+				<div class="col-md-4 col-12" >
 					<img class="gallery_img img-responsive"  :src="project.source">
 					<v-fade-transition>
 						<v-overlay v-if="hover" absolute :opacity="0.90" class="text-center">
 							<h1>{{project.name}}</h1>
-							<v-btn @click="alertMe" light data-toggle="modal" :data-target="'#modal'+ project.id" >click me for info </v-btn>
+							<v-btn  light data-toggle="modal" :data-target="'#modal'+ project.id" >click me for info </v-btn>
 
 						</v-overlay>
 					</v-fade-transition>
-					<div class="modal fade" :id="'#modal'+ project.id" tabindex="-1" role="dialog"  aria-hidden="true">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title">Area PE 1</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              
-            </div>
-          </div>
-        </div>  
+					<div class="modal fade bd-example-modal-lg" :id="'modal'+ project.id" tabindex="-1" role="dialog"  aria-hidden="true">
+						<div class="modal-dialog modal-lg" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h5 class="modal-title">{{ project.name }}</h5>
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+									</button>
+								</div>
+								<div class="modal-body">
+									<div class="container-fluid">
+										<div class="row">
+											<div class="col-4">
+												<img class="gallery_img img-responsive"  :src="project.source">	
+												
+											</div>
+
+											<div class="col-8">
+													<p><strong> insert some description here about your work</strong></p>
+													<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit nemo a sint aliquam perspiciatis, ea voluptas maxime dolorem eveniet omnis, reiciendis nobis commodi, laboriosam eos, tempore odio dignissimos molestiae nostrum.</p>
+												
+											</div>
+										</div> 
+									</div>
+
+								</div>
+								<div class="modal-footer">
+									<v-btn  data-dismiss="modal">Close</v-btn>
+								</div>
+							</div>
+						</div>
+					</div>  
 				</div>
 			</template>
 			<!-- <project-modal></project-modal> -->
@@ -54,9 +73,6 @@ export default {
 			setTimeout(()=> {
 				return false;
 			}, 2000)
-		},
-		alertMe(){
-			console.log('#modal' + this.project.id)
 		}
 	}
 	// },
@@ -68,13 +84,26 @@ export default {
 </script>
 
 <style scoped>
+	@font-face {
+		font-family: Nexa;
+		src: url(../../public/nexa.otf);
+}
+
+	@font-face {
+		font-family: Dense;
+		src: url(../../public/dense.otf);
+}
 	.gallery_img {
     width: 100%;
     height: 100%;
      object-fit: cover; 
-    
-
 }
+
+	h5 {
+		font-family: Nexa, sans-serif;
+		font-size: 1.3em;
+	}
+
 
 	
 </style>
